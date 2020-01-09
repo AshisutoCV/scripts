2c444eccf273166c184a6d956189223a5f4125c2
TakuyaARITA <tarita@ashisuto.co.jp>
2020-01-09 13:09:02 +0900

    dev-test

--- shield-setup.sh
+++ shield-setup.sh
@@ -22,7 +22,7 @@ BRANCH="Rel"
 ERICOMPASS="Ericom123$"
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 cd $CURRENT_DIR
-SCRIPTS_URL="https://ericom-tec.ashisuto.co.jp/shield"
+SCRIPTS_URL="https://ericom-tec.ashisuto.co.jp/shield/git/develop"
 
 if [ -f .es_branch ]; then
     BRANCH=$(cat .es_branch)
