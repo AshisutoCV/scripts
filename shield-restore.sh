525e8e98267228ae2ca37d13a88f1da46e6e0d0b
TakuyaARITA <tarita@ashisuto.co.jp>
2020-11-12 14:15:57 +0900

    fix shield-restore

--- shield-restore.sh
+++ shield-restore.sh
@@ -2,7 +2,7 @@
 
 ####################
 ### K.K. Ashisuto
-### VER=20200219a
+### VER=20201112a
 ####################
 export HOME=$(eval echo ~${SUDO_USER})
 export KUBECONFIG=${HOME}/.kube/config
@@ -10,7 +10,7 @@ export KUBECONFIG=${HOME}/.kube/config
 CONSUL_BACKUP_POD=$(kubectl get pods --namespace=management | grep consul-backup | awk {'print $1'})
 
 if [ $# != 0 ];then
-    TARGET_FILE="/consul/backup/$1"
+    TARGET_FILE="$1"
 else
     TARGET_FILE=$(kubectl exec -t --namespace=management ${CONSUL_BACKUP_POD} -- ls -1t /consul/backup/ | grep json | head -1)
 fi
